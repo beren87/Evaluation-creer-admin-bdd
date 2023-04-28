804770540313ef4aa4a6155c6f6572c32c67acf1
Berenger FERGUENIS <berenger@MacBook-Pro-de-Berenger.local>
2023-04-28 10:55:27 +0200

    master: Change AUTO_INCREMENT can be in one separate command

--- schemas/cinema.sql
+++ schemas/cinema.sql
@@ -1,35 +1,29 @@
 CREATE TABLE Cinema
 (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id INT PRIMARY KEY,
     nameCinema VARCHAR(255) NOT NULL,
     addressCinema VARCHAR(255) NOT NULL
 );
 
-ALTER TABLE Cinema AUTO_INCREMENT = 1;
-
 CREATE TABLE MovieTheater 
 (
-  id INT PRIMARY KEY AUTO_INCREMENT,
+  id INT PRIMARY KEY,
   numberSeats INT NOT NULL,
   cinema_id INT NOT NULL, 
   FOREIGN KEY (cinema_id) REFERENCES Cinema(id)
 );
 
-ALTER TABLE MovieTheater AUTO_INCREMENT = 1;
-
 CREATE TABLE Movie
 (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id INT PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
     timeMovie INT NOT NULL,
     minimumAge INT NOT NULL
 );
 
-ALTER TABLE Movie AUTO_INCREMENT = 1;
-
 CREATE TABLE Session 
 (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id INT PRIMARY KEY,
     openingHours DATETIME NOT NULL,
     movie_theater_id INT NOT NULL,
     movie_id INT NOT NULL,
@@ -37,15 +31,17 @@ CREATE TABLE Session
     FOREIGN KEY (movie_id) REFERENCES Movie(id)
 );
 
-ALTER TABLE Session AUTO_INCREMENT = 1;
-
 CREATE TABLE Booking
 (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id INT PRIMARY KEY,
     numberSeatsReserved INT NOT NULL,
     tariffs DECIMAL(5, 2) NOT NULL,
     session_id INT NOT NULL,
     FOREIGN KEY (session_id) REFERENCES Session(id)
 );
 
+ALTER TABLE Cinema AUTO_INCREMENT = 1;
+ALTER TABLE MovieTheater AUTO_INCREMENT = 1;
+ALTER TABLE Movie AUTO_INCREMENT = 1;
+ALTER TABLE Session AUTO_INCREMENT = 1;
 ALTER TABLE Booking AUTO_INCREMENT = 1;
